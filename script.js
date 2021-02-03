c5f6bfa2e289205256c1c6a3e8565cad8a30322b
kaidn28 <56955963+kaidn28@users.noreply.github.com>
2021-02-03 11:21:09 +0700

    trying some css but it seems not so good

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
 new Vue({
     el: '#app',
     data: {
-        player_health: 100,
-        monster_health: 100,
+        p1_health: 100,
+        p2_health: 100,
         game_started: false,
-        game_over: false
+        game_over: false,
+        turns: []
     },
 
     methods: {
 
         gameStart: function(){
             this.game_started = true;
-            this.player_health = 100; 
-            this.monster_health = 100;
+            this.game_over = false;
+            this.p1_health = 100; 
+            this.p2_health = 100;
         },
         healthUp: function(health){
-            if(health == 'player'){
-                this.player_health += 10
+            var hp = Math.floor(Math.random() * 11)+10
+            if(health == 'player1'){
+                this.p1_health += hp;
+                this.turns.unshift({
+                    isp1: true,
+                    text:'player 1 healed ' + hp + ' hp'})
             }
             else {
-                this.monster_health +=10
+                this.p2_health +=hp;
+                this.turns.unshift({
+                    isp1: false,
+                    text:'player 2 healed ' + hp + ' hp'})
             }
         },
         healthDown: function(health){
-            if(health == 'player'){
-                this.player_health -= 10
+            var hp = Math.floor(Math.random() * 11)+10
+            if(health == 'player1'){
+                this.p1_health -= hp
+                this.turns.unshift({
+                    isp1: false,
+                    text:'player 2 dealt ' + hp + ' damage'})
             }
             else {
-                this.monster_health -=10
+                this.p2_health -=hp
+                this.turns.unshift({
+                    isp1: true,
+                    text:'player 1 dealt ' + hp + ' damage'})
             }
             this.checkOver();
         },
         checkOver: function(){
-            if (this.player_health <=0 || this.monster_health <=0){
+            if (this.p1_health <=0 || this.p2_health <=0){
                 this.game_over = true;
                 setTimeout(function(that){that.game_started = false}, 3000, this);
             }
